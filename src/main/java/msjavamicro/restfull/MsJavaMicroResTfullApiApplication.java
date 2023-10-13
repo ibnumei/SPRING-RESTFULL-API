39a2dca58f507b36db2d9c2e6528678b31a1d776
IBNU FAUZI MEIRIYANTO <19058263@MACBJHCDITSD.local>
2023-10-13 08:49:20 +0700

    initialize project

---
+++ src/main/java/msjavamicro/restfull/MsJavaMicroResTfullApiApplication.java
@@ -0,0 +1,13 @@
+package msjavamicro.restfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsJavaMicroResTfullApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsJavaMicroResTfullApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msjavamicro/restfull/MsJavaMicroResTfullApiApplicationTests.java
@@ -0,0 +1,13 @@
+package msjavamicro.restfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsJavaMicroResTfullApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
